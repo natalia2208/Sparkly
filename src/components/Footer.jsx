bfcf3726db4624b082c0996807f89e7c376437a3
natalia2208 <collazosnatalia996@gmail.com>
2025-12-01 16:56:50 -0500

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-// import styles from "../styles/footer.module.css";
-
-// export default function Footer() {
-//     return (
-//         <footer className={styles.footer}>
-//             <p>2024 Mi aplicacion. Todos los derechos reservados</p>
-//         </footer>
-//     )
-// }
